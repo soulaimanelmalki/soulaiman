48c89c1cab91d8d3f0ae39ab8b56909d309dd398
soulaimanelmalki <45540257+soulaimanelmalki@users.noreply.github.com>
2018-12-02 18:34:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,479 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lottery of sweden</title>
+<!-- comments-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script src="js/popper.js"></script>
+	<script src="js/jquery.js"></script>
+	<script src="s4.min.js"></script>
+<!-- comments-->
+</head>
+<body>
+<header>
+<div id="lang">
+    <a href="registerfr.php">French</a>
+    </div>
+	<img src="se.png">
+	<h4 id="hash1">Electronic Diversity Visa<br> <span>Lottery of sweden</span></h4>
+	<h4 id="hash2">Electronic Diversity Visa<br> <span>
+Applicant Entry System: 12.4</span></h4>
+</header>
+
+	<h3 id="off">Electronic Diversity Visa Lottery</h3>
+	<div id="ads">
+		
+	</div>
+	<div id="cont1">
+		<p>Enter Entrant Information To register</p>
+	</div>
+<div id="glob">
+
+<form method="POST" action="reg_add.php">
+
+<div id="name">
+<div id="name1">
+<tr><td>1. first name and last name :</td>
+</tr>
+</div>
+<div id="name2">
+	<p>In the space below, type your first name and last name</p>	
+</div>
+<div id="name3">
+	<tr><td>
+	<input type="text" name="name" placeholder="Heli jaims ..."></td>
+	</tr>
+</div>
+</div>
+
+<!-- try  -->
+<div id="name">
+<div id="name1">
+<tr><td>2. Email :</td>
+</tr>
+</div>
+<div id="name2">
+	<p>In the space below, type your Email</p>	
+</div>
+<div id="name3">
+	<tr><td>
+	<input type="text" name="email" placeholder="Helijaims@gmail.com..."></td>
+	</tr>
+</div>
+</div>
+
+<div id="gender">
+	<div id="name1">
+<tr><td>3. gender :</td>
+</div>
+<div id="name2">
+	<p>Choose your gender</p>	
+</div>
+
+<td>
+	  <select name="gender">
+	  	<option>Male</option>
+	  	<option>Female</option>
+	  	<option>Other</option>
+	  </select>
+</td>
+	
+</tr>
+</div>
+
+<div id="birthday">
+	<div id="name1">
+<tr><td>4. Year of Birth :</td>
+</div>
+<div id="name2">
+	<p>Choose your birthday</p>	
+</div>
+<input type="date" name="birthday">
+</div>
+
+<div id="phone">
+	<div id="name1">
+<tr><td>5. phone number :</td>
+</div>
+<div id="name2">
+	<p>In the space below, type your phone number</p>	
+</div>
+ <input type="number" name="phone" placeholder="+74952411725">
+</div>
+
+
+<input id="submit" type="submit" name="submit" value="Submit">
+
+
+</form>
+
+</div>
+
+<!-- comments -->
+<div class="comment-area">
+		<div class="action">
+			<a class="like" href="javascript: void(0);">Like</a>
+			<a class="bubble" href="javascript: void(0);">Comment</a>
+			<a class="share-btn" href="javascript: void(0);">Share</a>
+		</div>
+		<div class="tot-likes">
+			<div class="like-imgs">
+				<div><img src="images/img2.jpg" width="32" height="32" /><img class="reaction-img" src="images/btn.png" width="16" height="16" /></div>
+				<div><img src="images/img1.jpg" width="32" height="32" /><img class="reaction-img" src="images/love.png" width="16" height="16" /></div>
+				<div><img src="images/img3.jpg" width="32" height="32" /><img class="reaction-img" src="images/btn.png" width="16" height="16" /></div>
+				<div><img src="images/img4.jpg" width="32" height="32" /><img class="reaction-img" src="images/btn.png" width="16" height="16" /></div>
+				<div><img src="images/img8.jpg" width="32" height="32" /><img class="reaction-img" src="images/btn.png" width="16" height="16" /></div>
+				<div><img src="images/img4.jpg" width="32" height="32" /><img class="reaction-img" src="images/btn.png" width="16" height="16" /></div>
+				<div><img src="images/img9.jpg" width="32" height="32" /><img class="reaction-img" src="images/love.png" width="16" height="16" /></div>
+				<div><img src="images/img5.jpg" width="32" height="32" /><img class="reaction-img" src="images/love.png" width="16" height="16" /></div>
+				<div><img src="images/img7.jpg" width="32" height="32" /><img class="reaction-img" src="images/btn.png" width="16" height="16" /></div>
+				<div><img src="images/img6.jpg" width="32" height="32" /><img class="reaction-img" src="images/love.png" width="16" height="16" /></div>
+			</div>
+
+
+	<p><span class="you">You And </span> 17,259 others like</p>
+		</div>
+		<div class="comment">
+			<img class="profile" src="images/1.png" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">Monu Ray</a>Thank you very much , I got the code</p>
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked">50</span>.
+				<span class="time">28 min</span>
+			</div>
+			<div class="comment">
+				<img class="profile" src="images/2.png" width="50" height="50" />
+				<p class="com-txt" style="margin-left: 35px;"><a class="name" href="javascript:void(0);">Pals Kelly</a>We wish all the migration and achieve his dream :)</p>
+
+				<div class="act"  style="margin-left: 35px;">
+					<a class="fblike" href="javascript:void(0);">Like</a> ·
+					<a href="javascript:void(0);">Reply</a> ·
+					<span class="liked">16</span>
+					<span class="time">20 min</span>
+				</div>
+			</div>
+			<div id="com3" class="comment hider">
+				<img class="profile" src="images/3.png" width="50" height="50" />
+				<p class="com-txt"  style="margin-left: 35px;"><a class="name" href="javascript:void(0);">Steven Resell</a> I hope I'm lucky seems that many people have gotten tickets </p>
+				<div class="act"  style="margin-left: 35px;">
+					<a class="fblike" href="javascript:void(0);">Like</a> ·
+					<a href="javascript:void(0);">Reply</a> ·
+					<span class="liked"></span>
+					<span class="time">Now</span>
+				</div>
+			</div>
+		</div>
+		<div class="comment">
+			<img class="profile" src="images/4.png" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">Jose Severino</a>It's great thank you</p>
+
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked">27</span>
+				<span class="time">7 Min</span>
+			</div>
+		</div>
+		<div class="comment">
+			<img class="profile" src="images/5.png" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">Ederson Goncalves</a>I got it.... Yeeaah !!!</p>
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked">18</span>
+				<span class="time">3 min</span>
+			</div>
+		</div>
+		<div id="com1" class="comment hider">
+			<img class="profile" src="images/6.png" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">Sidnéia Navarro</a> Code sent successfully Thank you </p>
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked likehide"></span>
+				<span class="time">Now</span>
+			</div>
+		</div>
+		<div id="com2" class="comment hider">
+			<img class="profile" src="images/7.png" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">جمانا الملك</a> Thanks  ❤ ...</p>
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked likehide"></span>
+				<span class="time">Now</span>
+			</div>
+			<div id="com5" class="comment hider">
+				<img class="profile" src="images/8.png" width="50" height="50" />
+				<p class="com-txt"  style="margin-left: 35px;"><a class="name" href="javascript:void(0);">Sandy Patel</a> Good Luck to Everyone :)</p>
+				<div class="act"  style="margin-left: 35px;">
+					<a class="fblike" href="javascript:void(0);">Like</a> ·
+					<a href="javascript:void(0);">Reply</a> ·
+					<span class="liked likehide"></span>
+					<span class="time">Now</span>
+				</div>
+			</div>
+		</div>
+		<div id="com4" class="comment hider">
+			<img class="profile" src="images/9.png" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">Maria Akther</a> that helpful thank you </p>
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked likehide"></span>
+				<span class="time">Now</span>
+			</div>
+		</div>
+		<div id="myCom" class="comment">
+			<img class="profile" src="images/generic.jpg" width="50" height="50" />
+			<p class="com-txt"><a class="name" href="javascript:void(0);">You</a>
+				<span id="myComment"></span></p>
+			<div class="act">
+				<a class="fblike" href="javascript:void(0);">Like</a> ·
+				<a href="javascript:void(0);">Reply</a> ·
+				<span class="liked likehide"></span>
+				<span class="time">Now</span>
+			</div>
+		</div>
+		<div class="addComment">
+			<img src="images/generic.jpg" width="50" height="50" />
+			<input type="text" id="addCom" placeholder="Add Comment...." />
+		</div>
+	</div>
+	<center style="margin-top: 25px;margin-bottom: 25px;">
+		All Rights Reserved to our company &copy; 2018
+	</center>
+
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+
+
+<!-- fin-->
+<footer>
+	<div id="page">
+	<p>
+	After you register in Electronic Diversity Visa Lottery of sweden,10,000 will be randomly selected from registrants
+	If you will be selected ,we will send a mail to your email address
+</p>
+</div>
+</footer>
+<style type="text/css">
+*{
+	padding: 0;
+	margin: 0;
+}
+header{
+
+	height: 3.3vw;
+	background:#2c3e50;
+}
+header img{
+	height: 2.3vw;
+	float: left;
+	margin-left: 7vw;
+	margin-top: .45vw;
+}
+header #hash1{
+	color: #FDFDFF;
+	font-size: 1.15vw;
+	text-align: center;
+	
+	padding-top: .3vw;
+	width: 16vw;
+	float: left;
+
+	
+}	
+header #hash1 span{
+	font-size: 1vw;
+}
+header #hash2{
+	margin-left: 73vw;
+	font-size: 1.15vw;
+	text-align: center;
+	padding-top: .3vw;
+	color: #FDFDFF;
+
+}
+header #hash2 span{
+	font-size: .8vw;
+}
+body #off{
+font-size: 1.7vw;
+ font-family: Times;
+ width: auto;
+ text-align: center;
+}
+body #cont1{
+	width: 81.5vw;
+	margin-left: 9vw;
+	height: 3vw;
+	background:#c7e4f3 ;
+	text-align: center;
+	line-height: 3vw;
+	color: #31708f;
+	font-size: 1.4vw;
+	font-weight: bold;
+}
+.gcont{
+	width: 81.5vw;
+	height: 40vw;
+	margin-left: 9vw;
+	border: .1vw solid #c7e4f3 ;
+}
+form{
+	width: 81.5vw;
+	margin-left: 3.5vw;
+	margin-bottom: 3vw;
+
+	}
+	form #name{
+		width: 73vw;
+		height: 8vw;
+		border: .15vw solid #2c3e50 ;
+		text-align: center;
+		font-size: 1.3vw;
+		margin-top: 1vw;
+	}
+	form #name1{
+		width: 73vw;
+		height: 2.5vw;
+		line-height: 2.5vw;
+		background: #2c3e50 ;
+		color: #FAFCFB;
+
+	}
+	form #name2{
+		margin: .4vw;
+		
+	}	
+	form #name3{
+		width: 73vw;
+		
+	}
+	form #name3 input{
+		font-size: 1vw;
+		width: 20vw;
+		height: 2.5vw;
+		text-align: center;
+	}
+form #gender{
+		margin-top: 1vw;
+		width: 73vw;
+		height: 8vw;
+		border: .15vw solid #2c3e50 ;
+		text-align: center;
+		font-size: 1.3vw;
+
+	}
+	form #gender option{
+		padding:0.3vw; 
+		font-size: 1vw;
+	}
+
+form #birthday{
+		margin-top: 1vw;
+		width: 73vw;
+		height: 8.5vw;
+		border: .15vw solid #2c3e50 ;
+		text-align: center;
+		font-size: 1.3vw;
+	}
+	form #birthday input{
+		width: 18vw;
+		font-size: 1.15vw;
+		text-align: center;
+		background: #2c3e50;
+		color: #FAF6F6;
+		height: 2.5vw;
+		margin: .4vw;
+	}
+	form #phone{
+		margin-top: 1vw;
+		width: 73vw;
+		height: 8.5vw;
+		border: .15vw solid #2c3e50 ;
+		text-align: center;
+		font-size: 1.3vw;
+	}
+	form #phone input{
+		margin: .4vw;
+		width: 20vw;
+		height: 2.5vw;
+		text-align: center;
+		font-size: 1vw;
+	}
+		form #submit{
+		margin-left: 34vw;
+		background:#17C136;
+		border: 0;
+		border-radius: .3vw;
+		color: #000801;
+		width: 6vw;
+		height: 1.5vw;
+		margin-top: .8vw;
+	}
+	form #submit:hover{
+		width: 6.3vw;
+		height: 1.7vw;
+	}
+form #cancel{
+		font-size: 1vw;
+		background:#547BC5;
+		border: 0;
+		border-radius: .3vw;
+		color: #000801;
+		width: 6vw;
+		height: 1.5vw;
+		margin-top: .8vw;
+		float: left;
+		margin-left: 1vw;
+		line-height:1.5vw;
+		text-align: center;
+	}
+		form #cancel:hover{
+		width: 6.3vw;
+		height: 1.7vw;
+	}
+a{   
+	text-decoration:none;
+	color: #000801;
+}
+
+#glob{
+	width: 81.28vw;
+	margin-left: 9vw;
+	border: .1vw solid #c7e4f3;
+}
+footer{
+	margin-top: 1vw;
+	background:#2c3e50;
+	
+	line-height: 3vw;
+	text-align: center;
+	font-size: 1.1vw;
+}
+
+#ads{
+	height: 12vw;
+}
+#lang{
+    margin-left:.8vw;
+    margin-top:.8vw;
+    float:left;
+    #FDFDFF
+}
+#lang a{
+    color:#FDFDFF;
+}
+</style>
+</body>
+</html>
